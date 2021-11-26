69c5fecc423d5b878a36e73b51a2d4f95262c2a2
Vladislav Antonyuk <vlad.antonyuk@gmail.com>
2021-11-27 00:01:28 +0200

    Infinite scroll

--- MauiAuth/Constants.cs
+++ MauiAuth/Constants.cs
@@ -2,7 +2,7 @@
 {
     public static class Constants
     {
-        public static readonly string ClientId = "YOUR_CLIENT_ID_HERE";
+        public static readonly string ClientId = System.Guid.Empty.ToString(); // "YOUR_CLIENT_ID_HERE"
         public static readonly string[] Scopes = new string[] { "openid", "offline_access" };
     }
 
