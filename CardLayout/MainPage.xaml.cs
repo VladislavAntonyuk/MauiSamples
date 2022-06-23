da812ad2cad6cac55c594e3ae999e1dec27ae0e0
Vladislav Antonyuk <vladislav.antonyuk@dataart.com>
2022-06-23 13:40:05 +0300

    CardLayout

---
+++ CardLayout/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CardLayout;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage(MainPageViewModel viewModel)
+	{
+		InitializeComponent();
+		BindingContext = viewModel;
+	}
+}
---
+++ CardLayout/MauiProgram.cs
@@ -0,0 +1,17 @@
+﻿namespace CardLayout;
+
+using Microsoft.Maui.Controls.Compatibility.Hosting;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder.UseMauiApp<App>().UseMauiCompatibility();
+
+		builder.Services.AddSingleton<MainPageViewModel>();
+		builder.Services.AddSingleton<MainPage>();
+		
+		return builder.Build();
+	}
+}
