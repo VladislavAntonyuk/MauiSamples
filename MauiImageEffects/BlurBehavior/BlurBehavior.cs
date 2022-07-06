ebf94a72bb96a2fe9967f9ed35eccb866609f347
Vladislav Antonyuk <vlad.antonyuk@gmail.com>
2022-07-06 14:25:22 +0300

    Blur behavior

---
+++ MauiImageEffects/BlurBehavior/BlurBehavior.cs
@@ -0,0 +1,23 @@
+﻿namespace MauiImageEffects.BlurBehavior;
+
+public partial class BlurBehavior
+{
+	public static readonly BindableProperty RadiusProperty = BindableProperty.Create(nameof(Radius), typeof(float), typeof(BlurBehavior), 10f, propertyChanged: OnRadiusChanged);
+
+	public float Radius
+	{
+		get => (float)GetValue(RadiusProperty);
+		set => SetValue(RadiusProperty, value);
+	}
+
+	static void OnRadiusChanged(BindableObject bindable, object oldValue, object newValue)
+	{
+		var behavior = (BlurBehavior)bindable;
+		if (behavior.imageView is null)
+		{
+			return;
+		}
+
+		SetRendererEffect(behavior.imageView, Convert.ToSingle(newValue));
+	}
+}
