54f6cade5464c0462a1323e1fb6c7455cc769e5e
Vladislav Antonyuk <vladislav.antonyuk@dataart.com>
2022-07-08 12:50:20 +0300

    SaturationBehavior

---
+++ MauiImageEffects/SaturationBehavior/SaturationBehavior.Android.cs
@@ -0,0 +1,44 @@
+﻿namespace MauiImageEffects.SaturationBehavior;
+
+using Android.Graphics;
+using Android.Widget;
+
+public partial class SaturationBehavior : PlatformBehavior<Image, ImageView>
+{
+	ImageView? imageView;
+	protected override void OnAttachedTo(Image bindable, ImageView platformView)
+	{
+		imageView = platformView;
+		SetRendererEffect(platformView, Saturation);
+	}
+
+	protected override void OnDetachedFrom(Image bindable, ImageView platformView)
+	{
+		SetRendererEffect(platformView, 0);
+	}
+
+	static void SetRendererEffect(ImageView imageView, float radius)
+	{
+		if (OperatingSystem.IsAndroidVersionAtLeast(31))
+		{
+			var renderEffect = radius > 0 ? GetEffect(radius) : null;
+			imageView.SetRenderEffect(renderEffect);
+		}
+		else
+		{
+
+		}
+	}
+
+	static RenderEffect? GetEffect(float saturation)
+	{
+		if (OperatingSystem.IsAndroidVersionAtLeast(31))
+		{
+			var colorMatrix = new ColorMatrix();
+			colorMatrix.SetSaturation(saturation);
+			return RenderEffect.CreateColorFilterEffect(new ColorMatrixColorFilter(colorMatrix));
+		}
+
+		return null;
+	}
+}
---
+++ MauiImageEffects/SaturationBehavior/SaturationBehavior.iOS.cs
@@ -0,0 +1,52 @@
+﻿using UIKit;
+
+namespace MauiImageEffects.SaturationBehavior;
+
+using CoreGraphics;
+using CoreImage;
+
+public partial class SaturationBehavior : PlatformBehavior<Image, UIImageView>
+{
+	private CGImage? originalImage;
+	UIImageView? imageView;
+
+	protected override void OnAttachedTo(Image bindable, UIImageView platformView)
+	{
+		imageView = platformView;
+
+		originalImage = platformView.Image?.CGImage;
+		SetRendererEffect(imageView, Saturation);
+	}
+
+	protected override void OnDetachedFrom(Image bindable, UIImageView platformView)
+	{
+		SetImage(platformView, originalImage);
+	}
+
+	static void SetImage(UIImageView imageView, CGImage? image)
+	{
+		if (image is null)
+		{
+			return;
+		}
+
+		imageView.Image = new UIImage(image);
+	}
+
+	static void SetRendererEffect(UIImageView imageView, float saturation)
+	{
+		if (imageView.Image?.CGImage is null)
+		{
+			return;
+		}
+
+		var myContext = CIContext.Create();
+		var inputImage = CIImage.FromCGImage(imageView.Image.CGImage);
+		var filter = new CISaturationBlendMode()
+		{
+			InputImage = inputImage
+		};
+		var resultImage = myContext.CreateCGImage(filter.OutputImage!, inputImage.Extent);
+		SetImage(imageView, resultImage);
+	}
+}
