1eeee5353819da4dcccd8ddcc9f370c1b319ab26
Vladislav Antonyuk <vlad.antonyuk@gmail.com>
2022-08-09 03:42:41 +0300

    iOSExtensions

---
+++ iOSExtensions/AppContainer/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace AppContainer;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private async void OnClicked(object sender, EventArgs e)
+	{
+		await Launcher.OpenAsync("https://vladislavantonyuk.azurewebsites.net/");
+	}
+}
+
