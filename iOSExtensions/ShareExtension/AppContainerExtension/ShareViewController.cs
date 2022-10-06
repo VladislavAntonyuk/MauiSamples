4f83cd1b050fc869bec962016293a4efcf526a6c
Vladislav Antonyuk <vlad.antonyuk@gmail.com>
2022-10-06 22:03:36 +0300

    Fixes for .NET 7

--- iOSExtensions/ShareExtension/AppContainerExtension/ShareViewController.cs
+++ iOSExtensions/ShareExtension/AppContainerExtension/ShareViewController.cs
@@ -36,13 +36,13 @@ namespace AppContainerExtension
 			// This is called after the user selects Post. Do the upload of contentText and/or NSExtensionContext attachments.
 
 			// Inform the host that we're done, so it un-blocks its UI. Note: Alternatively you could call super's -didSelectPost, which will similarly complete the extension context.
-			ExtensionContext.CompleteRequest (new NSExtensionItem [0], null);
+			ExtensionContext?.CompleteRequest (Array.Empty<NSExtensionItem>(), null);
 		}
 
 		public override SLComposeSheetConfigurationItem[] GetConfigurationItems ()
 		{
 			// To add configuration options via table cells at the bottom of the sheet, return an array of SLComposeSheetConfigurationItem here.
-			return new SLComposeSheetConfigurationItem[0];
+			return Array.Empty<SLComposeSheetConfigurationItem>();
 		}
 	}
 }
