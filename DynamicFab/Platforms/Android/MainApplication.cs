0b2dd4550b658e0b7dcd58d0324d149a3c9687bc
Vladislav Antonyuk <vlad.antonyuk@gmail.com>
2022-11-14 15:25:02 +0200

    FAB

---
+++ DynamicFab/Platforms/Android/MainApplication.cs
@@ -0,0 +1,16 @@
+﻿namespace DynamicFab
+{
+	using Android.App;
+	using Android.Runtime;
+
+	[Application]
+	public class MainApplication : MauiApplication
+	{
+		public MainApplication(IntPtr handle, JniHandleOwnership ownership)
+			: base(handle, ownership)
+		{
+		}
+
+		protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+	}
+}
---
+++ DynamicFab/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿namespace DynamicFab
+{
+	using Foundation;
+
+	[Register("AppDelegate")]
+	public class AppDelegate : MauiUIApplicationDelegate
+	{
+		protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+	}
+}
