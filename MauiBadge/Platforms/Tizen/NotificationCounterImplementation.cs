d284d0f6ea36dd1d5c9a6fd0511422a270c81d4d
Vladislav Antonyuk <vlad.antonyuk@gmail.com>
2022-11-29 12:35:39 +0200

    Add Tizen

--- MauiBadge/Platforms/Tizen/NotificationCounterImplementation.cs
+++ MauiBadge/Platforms/Tizen/NotificationCounterImplementation.cs
@@ -1,9 +1,29 @@
 ﻿namespace MauiBadge;
 
+using Tizen.Applications;
+using Tizen.NUI;
+
 public class NotificationCounterImplementation : INotificationCounter
 {
 	public void SetNotificationCount(int count)
 	{
-		throw new NotImplementedException();
+		var appId = Application.Current.ApplicationInfo.ApplicationId;
+		if (count is 0)
+		{
+			BadgeControl.Remove(appId);
+			return;
+		}
+		
+		var badge = BadgeControl.Find(appId);
+		if (badge is null)
+		{
+			badge = new Badge(appId, count);
+			BadgeControl.Add(badge);
+		}
+		else
+		{
+			badge.Count = count;
+			BadgeControl.Update(badge);
+		}
 	}
 }
