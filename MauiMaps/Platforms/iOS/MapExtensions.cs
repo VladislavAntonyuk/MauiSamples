6da5c38efafebba180ab1100f501d46df51498ab
Vladislav Antonyuk <vlad.antonyuk@gmail.com>
2022-12-03 15:00:47 +0200

    Fix Apple MarkerClicked, InfoWindowClicked events

--- MauiMaps/Platforms/iOS/MapExtensions.cs
+++ MauiMaps/Platforms/iOS/MapExtensions.cs
@@ -1,12 +1,17 @@
 ﻿namespace MauiMaps;
 
+using System;
 using CoreLocation;
 using MapKit;
 using Microsoft.Maui.Controls.Compatibility.Platform.iOS;
+using Microsoft.Maui.Maps;
 using Microsoft.Maui.Maps.Platform;
+using UIKit;
 
 public static partial class MapExtensions
 {
+	private static UIView? lastTouchedView;
+
 	public static async Task AddAnnotation(this CustomPin pin)
 	{
 		var imageSourceHandler = new ImageLoaderSourceHandler();
@@ -18,7 +23,9 @@ public static partial class MapExtensions
 			Title = pin.Label,
 			Subtitle = pin.Address,
 			Coordinate = new CLLocationCoordinate2D(pin.Location.Latitude, pin.Location.Longitude),
+			Pin = pin
 		};
+		pin.MarkerId = annotation;
 
 		var nativeMap = (MauiMKMapView?)pin.Map?.Handler?.PlatformView;
 		if (nativeMap is not null)
@@ -28,6 +35,14 @@ public static partial class MapExtensions
 			nativeMap.GetViewForAnnotation += GetViewForAnnotations;
 			nativeMap.AddAnnotation(annotation);
 		}
+	}
+
+	private static void OnCalloutClicked(IMKAnnotation annotation)
+	{
+		var pin = GetPinForAnnotation(annotation);
+		if (lastTouchedView is MKAnnotationView)
+			return;
+		pin?.SendInfoWindowClick();
 	}
 
 	private static MKAnnotationView GetViewForAnnotations(MKMapView mapView, IMKAnnotation annotation)
@@ -42,6 +57,42 @@ public static partial class MapExtensions
 			annotationView.CanShowCallout = true;
 		}
 
-		return annotationView ?? new MKAnnotationView(annotation, null);
+		var result = annotationView ?? new MKAnnotationView(annotation, null);
+		AttachGestureToPin(result, annotation);
+		return result;
+	}
+
+	static void AttachGestureToPin(MKAnnotationView mapPin, IMKAnnotation annotation)
+	{
+		var recognizers = mapPin.GestureRecognizers;
+
+		if (recognizers != null)
+		{
+			foreach (var r in recognizers)
+			{
+				mapPin.RemoveGestureRecognizer(r);
+			}
+		}
+
+		var recognizer = new UITapGestureRecognizer(g => OnCalloutClicked(annotation))
+		{
+			ShouldReceiveTouch = (gestureRecognizer, touch) =>
+			{
+				lastTouchedView = touch.View;
+				return true;
+			}
+		};
+
+		mapPin.AddGestureRecognizer(recognizer);
+	}
+
+	static IMapPin? GetPinForAnnotation(IMKAnnotation? annotation)
+	{
+		if (annotation is CustomAnnotation customAnnotation)
+		{
+			return customAnnotation.Pin;
+		}
+
+		return null;
 	}
 }
