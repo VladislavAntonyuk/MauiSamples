810250f6b660dbb9c774eef010e67d0a94560951
Vladislav Antonyuk <vlad.antonyuk@gmail.com>
2022-12-04 22:12:37 +0200

    RepositoryContributors

---
+++ RepositoryContributors/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace RepositoryContributors
+{
+	public partial class App : Application
+	{
+		public App()
+		{
+			InitializeComponent();
+
+			MainPage = new MainPage();
+		}
+	}
+}
---
+++ RepositoryContributors/MauiProgram.cs
@@ -0,0 +1,26 @@
+﻿namespace RepositoryContributors
+{
+	using Microsoft.Extensions.Logging;
+	using RepositoryContributors.Data;
+
+	public static class MauiProgram
+	{
+		public static MauiApp CreateMauiApp()
+		{
+			var builder = MauiApp.CreateBuilder();
+			builder
+				.UseMauiApp<App>();
+
+			builder.Services.AddMauiBlazorWebView();
+
+#if DEBUG
+		builder.Services.AddBlazorWebViewDeveloperTools();
+		builder.Logging.AddDebug();
+#endif
+
+			//builder.Services.AddSingleton<WeatherForecastService>();
+
+			return builder.Build();
+		}
+	}
+}
