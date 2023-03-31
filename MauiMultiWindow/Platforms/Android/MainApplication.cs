f620a7b2e045965628d519d3d8a4406ae323b499
Vladislav Antonyuk <vlad.antonyuk@gmail.com>
2023-03-31 13:40:47 -0700

    update thank you

--- MauiMultiWindow/Platforms/Android/MainApplication.cs
+++ MauiMultiWindow/Platforms/Android/MainApplication.cs
@@ -10,8 +10,5 @@ public class MainApplication : MauiApplication
 	{
 	}
 
-	protected override MauiApp CreateMauiApp()
-	{
-		return MauiProgram.CreateMauiApp();
-	}
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
 }
