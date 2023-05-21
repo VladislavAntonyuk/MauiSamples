400ea21edc347eebae2c4253fd2e20ca50b7512a
Vladislav Antonyuk <vlad.antonyuk@gmail.com>
2023-05-21 23:31:17 +0300

    DraggableView

---
+++ MauiDraggableView/Platforms/Android/DraggableViewHandler.cs
@@ -0,0 +1,64 @@
+﻿namespace MauiDraggableView;
+
+using Android.Views;
+using Microsoft.Maui;
+using Microsoft.Maui.Handlers;
+using Microsoft.Maui.Platform;
+
+public class DraggableViewHandler : ViewHandler<DraggableView, View>
+{
+	private float deltaX;
+	private float deltaY;
+
+	public DraggableViewHandler(IPropertyMapper mapper, CommandMapper? commandMapper = null) : base(mapper, commandMapper)
+	{
+	}
+
+	public DraggableViewHandler() : this(ViewMapper, ViewCommandMapper)
+	{
+	}
+
+	protected override void ConnectHandler(View platformView)
+	{
+		base.ConnectHandler(platformView);
+		platformView.Touch += HandleTouch;
+	}
+
+	protected override void DisconnectHandler(View platformView)
+	{
+		platformView.Touch -= HandleTouch;
+		base.DisconnectHandler(platformView);
+	}
+
+	protected override View CreatePlatformView()
+	{
+		return VirtualView.Content.ToPlatform(MauiContext ?? throw new NullReferenceException());
+	}
+
+
+	private void HandleTouch(object? sender, View.TouchEventArgs args)
+	{
+		var e = args.Event;
+		if (e is null)
+		{
+			return;
+		}
+
+		float x = e.RawX;
+		float y = e.RawY;
+		switch (e.Action)
+		{
+			case MotionEventActions.Down:
+				deltaX = x - PlatformView.GetX();
+				deltaY = y - PlatformView.GetY();
+				break;
+			case MotionEventActions.Move:
+				var newX = x - deltaX;
+				var newY = y - deltaY;
+				PlatformView.SetX(newX);
+				PlatformView.SetY(newY);
+
+				break;
+		}
+	}
+}
---
+++ MauiDraggableView/Platforms/Windows/DraggableViewHandler.cs
@@ -0,0 +1,81 @@
+﻿namespace MauiDraggableView;
+
+using Windows.Foundation;
+using Microsoft.Maui.Handlers;
+using Microsoft.Maui.Platform;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+
+public class DraggableViewHandler : ViewHandler<DraggableView, FrameworkElement>
+{
+	private bool isPointerPressed;
+	private Point previousPoint;
+
+	public DraggableViewHandler(IPropertyMapper mapper, CommandMapper? commandMapper = null) : base(mapper, commandMapper)
+	{
+	}
+
+	public DraggableViewHandler() : this(ViewMapper, ViewCommandMapper)
+	{
+	}
+
+	protected override void ConnectHandler(FrameworkElement platformView)
+	{
+		base.ConnectHandler(platformView);
+		platformView.PointerPressed += PlatformViewPointerPressed;
+		platformView.PointerMoved += PlatformViewOnPointerMoved;
+		platformView.PointerReleased += PlatformViewPointerReleased;
+	}
+
+	private void PlatformViewPointerReleased(object sender, PointerRoutedEventArgs e)
+	{
+		isPointerPressed = false;
+		PlatformView.ReleasePointerCapture(e.Pointer);
+		e.Handled = true;
+	}
+
+	private void PlatformViewPointerPressed(object sender, PointerRoutedEventArgs e)
+	{
+		isPointerPressed = true;
+		previousPoint = e.GetCurrentPoint(PlatformView).Position;
+		PlatformView.CapturePointer(e.Pointer);
+		e.Handled = true;
+	}
+
+	private void PlatformViewOnPointerMoved(object sender, PointerRoutedEventArgs e)
+	{
+		if (isPointerPressed)
+		{
+			var currentPoint = e.GetCurrentPoint(PlatformView).Position;
+			var offsetX = currentPoint.X - previousPoint.X;
+			var offsetY = currentPoint.Y - previousPoint.Y;
+
+			var transform = PlatformView.RenderTransform as CompositeTransform;
+			if (transform == null)
+			{
+				transform = new CompositeTransform();
+				PlatformView.RenderTransform = transform;
+			}
+
+			transform.TranslateX += offsetX;
+			transform.TranslateY += offsetY;
+
+			previousPoint = currentPoint;
+			e.Handled = true;
+		}
+	}
+
+	protected override void DisconnectHandler(FrameworkElement platformView)
+	{
+		platformView.PointerPressed -= PlatformViewPointerPressed;
+		platformView.PointerMoved -= PlatformViewOnPointerMoved;
+		platformView.PointerReleased -= PlatformViewPointerReleased;
+		base.DisconnectHandler(platformView);
+	}
+
+	protected override FrameworkElement CreatePlatformView()
+	{
+		return VirtualView.Content.ToPlatform(MauiContext ?? throw new NullReferenceException());
+	}
+}
