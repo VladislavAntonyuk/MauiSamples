a7dd86ac87f1b13132379a17c2e7121aa72505c4
Jeú <jeufrancosilva@gmail.com>
2023-10-19 16:35:39 -0300

    Update AppDbContext.cs

--- task-list-app-maui/Data/AppDbContext.cs
+++ task-list-app-maui/Data/AppDbContext.cs
@@ -4,12 +4,7 @@ using task_list_app_maui.Models;
 
 namespace task_list_app_maui.Data;
 
-public class AppDbContext : DbContext
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
 {
-    public AppDbContext(DbContextOptions<AppDbContext> options)
-        : base(options)
-    {
-    }
-
-    public DbSet<Tarefa> Tarefas { get; set; }
+	public DbSet<Tarefa> Tarefas { get; set; }
 }
