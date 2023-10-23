322d29607862d9ae9ceea7d2ed100f565abc6465
Jeú <jeufrancosilva@gmail.com>
2023-10-23 17:21:35 -0300

    Update AdicionarTarefaView.xaml.cs

--- task-list-app-maui/Views/AdicionarTarefaView.xaml.cs
+++ task-list-app-maui/Views/AdicionarTarefaView.xaml.cs
@@ -2,22 +2,24 @@ using task_list_app_maui.ViewModels;
 
 namespace task_list_app_maui.Views;
 
-public partial class AdicionarTarefaView : ContentPage
+public partial class EditarTarefaView : ContentPage
 {
 	private readonly TarefasViewModel tarefasViewModel;
 
-	public AdicionarTarefaView(TarefasViewModel tarefasViewModel)
+	public EditarTarefaView(TarefasViewModel tarefasViewModel)
 	{
 		InitializeComponent();
 
 		this.tarefasViewModel = tarefasViewModel;
 
 		BindingContext = this.tarefasViewModel;
+
+		this.tarefasViewModel.EditModeOn();
 	}
 
 	private async void OnSaveTaskClicked(object sender, EventArgs e)
 	{
-		tarefasViewModel.Create();
+		tarefasViewModel.Edit();
 
 		if (!string.IsNullOrEmpty(tarefasViewModel.HasErrorsCodeBehind))
 		{
@@ -31,8 +33,10 @@ public partial class AdicionarTarefaView : ContentPage
 		}
 	}
 
-	private async void OnCloseAddTaskClicked(object sender, EventArgs e)
+	private async void OnCloseEditTaskClicked(object sender, EventArgs e)
 	{
+		tarefasViewModel.EditModeOff();
+
 		await Navigation.PopModalAsync();
 	}
 }
