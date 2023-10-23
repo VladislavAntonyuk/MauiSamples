0d80c5f163dab841b355604386c3c2eb01575fb4
Jeú <jeufrancosilva@gmail.com>
2023-10-23 17:23:20 -0300

    Update TarefasConcluidasView.xaml.cs

--- task-list-app-maui/Views/TarefasConcluidasView.xaml.cs
+++ task-list-app-maui/Views/TarefasConcluidasView.xaml.cs
@@ -5,34 +5,34 @@ namespace task_list_app_maui.Views;
 
 public partial class TarefasConcluidasView : ContentPage
 {
-	private TarefasViewModel _tarefasViewModel;
+	private readonly TarefasViewModel tarefasViewModel;
 
 	public TarefasConcluidasView(TarefasViewModel tarefasViewModel)
 	{
 		InitializeComponent();
 
-		_tarefasViewModel = tarefasViewModel;
-		_tarefasViewModel.GetAll();
+		this.tarefasViewModel = tarefasViewModel;
+		this.tarefasViewModel.GetAll();
 
-		BindingContext = _tarefasViewModel;
+		BindingContext = this.tarefasViewModel;
 	}
 
 	private async void OnEditClicked(object sender, EventArgs e)
 	{
 		var imageButton = (ImageButton)sender;
-    	var tarefa = (Tarefa)imageButton.BindingContext;
+		var tarefa = (Tarefa)imageButton.BindingContext;
 
 		CollectionViewTarefasConcluidas.SelectedItem = tarefa;
 
 		if (CollectionViewTarefasConcluidas.SelectedItem == null) return;
 
-		await Navigation.PushModalAsync(new EditarTarefaView(_tarefasViewModel));
+		await Navigation.PushModalAsync(new EditarTarefaView(tarefasViewModel));
 	}
 
 	private async void OnDeleteClicked(object sender, EventArgs e)
 	{
 		var imageButton = (ImageButton)sender;
-    	var tarefa = (Tarefa)imageButton.BindingContext;
+		var tarefa = (Tarefa)imageButton.BindingContext;
 
 		CollectionViewTarefasConcluidas.SelectedItem = tarefa;
 
@@ -41,15 +41,15 @@ public partial class TarefasConcluidasView : ContentPage
 		bool answer = await DisplayAlert(Title, "Do you confirm the deletion of this task?", "Yes", "No");
 		if (!answer) return;
 
-		_tarefasViewModel.Delete();
+		tarefasViewModel.Delete();
 
-		if (!string.IsNullOrEmpty(_tarefasViewModel.HasErrorsCodeBehind))
-        {
-            await DisplayAlert(Title, _tarefasViewModel.HasErrorsCodeBehind, "OK");
-        }
-        else
-        {
-			_tarefasViewModel.GetAll();
-        }
+		if (!string.IsNullOrEmpty(tarefasViewModel.HasErrorsCodeBehind))
+		{
+			await DisplayAlert(Title, tarefasViewModel.HasErrorsCodeBehind, "OK");
+		}
+		else
+		{
+			tarefasViewModel.GetAll();
+		}
 	}
 }
