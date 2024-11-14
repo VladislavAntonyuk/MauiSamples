f0d01a4b272213e05e02d599a648a1609a1360de
Vladislav Antonyuk <vladislav.antonyuk@dataart.com>
2024-11-14 15:12:30 +0200

    remove tizen

--- MauiMultiWindow/Platforms/Tizen/WindowExtensions.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MauiMultiWindow;
-
-public static class WindowExtensions
-{
-	public static Task<T?> OpenModalWindowAsync<T>(this Window parentWindow, ModalWindow<T> content)
-	{
-		return Task.FromResult<T?>(default);
-	}
-}
