1e9f485e21044b19e95e8ef0367ccc2060797ad1
Vladislav Antonyuk <vlad.antonyuk@gmail.com>
2025-07-16 16:38:03 +0300

    update to night mode support

--- MauiIpCamera/Platforms/Android/LocalIpService.cs
+++ MauiIpCamera/Platforms/Android/LocalIpService.cs
@@ -2,24 +2,56 @@
 
 using System.Net;
 using Android.Content;
+using Android.Net;
 using Android.Net.Wifi;
+using Java.Net;
 
 public class LocalIpService : ILocalIpService
 {
 	public IPAddress GetLocalIpAddress()
 	{
-		var context = Android.App.Application.Context;
-		var wifiManager = (WifiManager)context.GetSystemService(Context.WifiService);
-		var ip = wifiManager?.ConnectionInfo?.IpAddress ?? 0;
+		if (OperatingSystem.IsAndroidVersionAtLeast(23))
+		{
+			var connectivityManager = (ConnectivityManager?)Android.App.Application.Context.GetSystemService(Context.ConnectivityService);
 
-		var ipAddress = string.Format(
-			"{0}.{1}.{2}.{3}",
-			(ip & 0xFF),
-			(ip >> 8) & 0xFF,
-			(ip >> 16) & 0xFF,
-			(ip >> 24) & 0xFF
-		);
+			var activeNetwork = connectivityManager?.ActiveNetwork;
+			if (activeNetwork == null)
+			{
+				return IPAddress.Loopback;
+			}
 
-		return IPAddress.Parse(ipAddress);
+			var linkProperties = connectivityManager?.GetLinkProperties(activeNetwork);
+			if (linkProperties?.LinkAddresses == null)
+			{
+				return IPAddress.Loopback;
+			}
+
+			foreach (var linkAddress in linkProperties.LinkAddresses)
+			{
+				var address = linkAddress.Address;
+				if (address is not Inet4Address || address.IsLoopbackAddress)
+				{
+					continue;
+				}
+
+				var hostAddress = address.HostAddress;
+				if (!string.IsNullOrEmpty(hostAddress) && IPAddress.TryParse(hostAddress, out var ipAddress))
+				{
+					return ipAddress;
+				}
+			}
+		}
+		else
+		{
+			var context = Android.App.Application.Context;
+			var wifiManager = (WifiManager?)context.GetSystemService(Context.WifiService);
+			var ip = wifiManager?.ConnectionInfo?.IpAddress ?? 0;
+
+			var ipAddress = $"{(ip & 0xFF)}.{(ip >> 8) & 0xFF}.{(ip >> 16) & 0xFF}.{(ip >> 24) & 0xFF}";
+
+			return IPAddress.Parse(ipAddress);
+		}
+
+		return IPAddress.Loopback;
 	}
 }
